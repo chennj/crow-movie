dc8d3f6a05113b28a89fddf5fffc39f41a4e0b05
chennj <chennj_cn@aliyun.com>
2020-06-01 17:41:28 +0800

    update

--- crow-movie-user/src/main/java/org/crow/movie/user/web/api/member/pc/MemberPcApi.java
+++ crow-movie-user/src/main/java/org/crow/movie/user/web/api/member/pc/MemberPcApi.java
@@ -1,5 +1,6 @@
 package org.crow.movie.user.web.api.member.pc;
 
+import java.io.File;
 import java.io.UnsupportedEncodingException;
 import java.text.DateFormat;
 import java.text.ParseException;
@@ -9,11 +10,14 @@ import java.util.Date;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.crow.movie.user.common.ApplicationProperties;
 import org.crow.movie.user.common.db.entity.MemberInfo;
 import org.crow.movie.user.common.db.entity.MemberOpen;
 import org.crow.movie.user.common.db.model.ReturnT;
 import org.crow.movie.user.common.db.service.MemberInfoService;
 import org.crow.movie.user.common.db.service.MemberOpenService;
+import org.crow.movie.user.common.plugin.qrcode.QRCodeService;
+import org.crow.movie.user.common.util.CommUtil;
 import org.crow.movie.user.common.util.DigestUtils;
 import org.crow.movie.user.common.util.IPUtil;
 import org.crow.movie.user.common.util.RegexUtil;
@@ -38,7 +42,12 @@ public class MemberPcApi extends BasePcController{
 	@Autowired
 	private MemberOpenService memberOpenService;
 	
-	@SuppressWarnings("deprecation")
+	@Autowired
+    private QRCodeService qrCodeService;
+	
+	@Autowired
+    private ApplicationProperties appProperties;
+	
 	@RequestMapping(value="login-account", method=RequestMethod.POST)
 	public ReturnT<?> loginAccount(
 			HttpServletRequest request, 
@@ -104,9 +113,53 @@ public class MemberPcApi extends BasePcController{
 		}
 		memberOpenService.add(memberOpen);
 		
+		if (StrUtil.isEmpty(member.getPromoQrcode())){
+			
+			int width 	= 166;
+			int height 	= 166;
+			
+			String qrcodeDir = appProperties.getQrcodeDir();
+			String uploadDir = appProperties.getUploadDir();
+			String promo_url = CommUtil.getDomain()+"?code=";
+	        
+			String data = promo_url+member.getPromoCode();
+			String imageName = String.format("%d_qr.png", member.getId());
+	        File qrFile = new File(qrcodeDir, imageName);
+	        qrCodeService.encode(data, width, height, qrFile);
+	        
+	        logger.info("生成的二维码文件:{}", qrFile.getAbsolutePath());
+	        
+	        File logoFile = new File(uploadDir, "logo.png");
+	        imageName = String.format("%s_qr_logo.png", member.getId());
+	        File logoQrFile = new File(qrcodeDir, imageName);
+	        qrCodeService.encodeWithLogo(qrFile, logoFile, logoQrFile);
+	        logger.info("生成的带logo二维码文件:{}", logoQrFile.getAbsolutePath());
+	        
+	        String promo_filename = logoQrFile.getName();
+	        member.setPromoQrcode(promo_filename);
+	        memberInfoService.modify(member);
+
+		}
 		JSONObject jRet = new JSONObject();
 		jRet.put("user_id", member.getId());
+		jRet.put("nick_name", member.getNickName());
+		jRet.put("is_visitor", member.getIsVisitor());
+		jRet.put("is_vip", 2);
+		jRet.put("promo_code", member.getPromoCode());
+		jRet.put("promo_qrcode", CommUtil.getHost(member.getPromoQrcode()));
+		jRet.put("promo_url", member.getPromoCode());
+		jRet.put("day_view_times", member.getDayViewTimes());
+		jRet.put("today_view_times", member.getTodayViewTimes());
+		jRet.put("re_today_view_times", member.getReTodayViewTimes());
 		jRet.put("accessToken", TokenUtil.genToken(member.getAccount(), member.getId()));
+		
+		if (member.getExpireTime() > now()){
+			jRet.put("is_vip", 1);
+			jRet.put("expire_time", member.getExpireTime());
+			
+			jRet.put("today_view_times", "无限");
+			jRet.put("re_today_view_times", "无限");
+		}
 		return success(jRet);
 	}
 	
